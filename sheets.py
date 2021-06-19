8c629551af0608cd65022214974eb0533e6b039d
Daniel <41345110+zazu7765@users.noreply.github.com>
2021-06-18 22:30:28 -0400

    port is now 80
    Signed-off-by: Daniel <41345110+zazu7765@users.noreply.github.com>

--- sheets.py
+++ sheets.py
@@ -1,12 +1,13 @@
 import csv
 
 import ezsheets
-
+import json
+config = json.load(open('config.json'))
 
 def sheetinit():
-    ss = ezsheets.Spreadsheet('1iA6Wh31JKgqBVflHOb-gidD91Ru6D43ntzGqHpdLGOI')  # grab the google spreadsheet id
+    ss = ezsheets.Spreadsheet(config['spreadsheet'])  # grab the google spreadsheet id
     ss.downloadAsCSV()
-    sheetfilecsv = open('Coding_CFC_Classes_Sign_Up_.csv')
+    sheetfilecsv = open('CFC_Classes_Sign_Up_.csv')
     sheetdatacsv = csv.reader(sheetfilecsv)
     dataList = list(sheetdatacsv)
     dataList.pop(0)
@@ -14,7 +15,7 @@ def sheetinit():
 
 
 def courseinit():
-    ss = ezsheets.Spreadsheet('1iA6Wh31JKgqBVflHOb-gidD91Ru6D43ntzGqHpdLGOI')  # grab the google spreadsheet id
+    ss = ezsheets.Spreadsheet(config['spreadsheet'])  # grab the google spreadsheet id
     sh = ss.sheets[4]
     courses = sh.getColumn(1)
     return courses
